ecc0d06200b4f681a46ad99657022bc1ac368eb7
Bimal Shrestha <69587963+bimalsh01@users.noreply.github.com>
2023-08-25 10:03:18 +0545

    fix protected route

--- src/protected/AdminRoute.jsx
+++ src/protected/AdminRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 
 const AdminRoute = () => {
-  const user = localStorage.getItem('user')
+  const user = JSON.parse(localStorage.getItem('user'))
   return user !== null && user.isAdmin? <Outlet/> : <Navigate to='/login'/>
 }
 
--- src/protected/UserRoute.jsx
+++ src/protected/UserRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Outlet, Navigate } from 'react-router-dom'
 
 const UserRoute = () => {
-    const user = localStorage.getItem('user')
+    const user = JSON.parse(localStorage.getItem('user'))
     return user !== null ? <Outlet/> : <Navigate to='/login'/>
 }
 
