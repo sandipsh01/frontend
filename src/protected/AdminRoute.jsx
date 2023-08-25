61c8f846e37efde0718c6597b3466be3f378ceb7
Bimal Shrestha <69587963+bimalsh01@users.noreply.github.com>
2023-08-25 10:13:51 +0545

    fix

--- src/protected/AdminRoute.jsx
+++ src/protected/AdminRoute.jsx
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from 'react-router-dom'
 
 const AdminRoute = () => {
   const user = JSON.parse(localStorage.getItem('user'))
-  return user !== null && user.isAdmin? <Outlet/> : <Navigate to='/login'/>
+  return user != null && user.isAdmin? <Outlet/> : <Navigate to='/login'/>
 }
 
 export default AdminRoute
--- src/protected/UserRoute.jsx
+++ src/protected/UserRoute.jsx
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from 'react-router-dom'
 
 const UserRoute = () => {
     const user = JSON.parse(localStorage.getItem('user'))
-    return user !== null ? <Outlet/> : <Navigate to='/login'/>
+    return user != null ? <Outlet/> : <Navigate to='/login'/>
 }
 
 export default UserRoute
